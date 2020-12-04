a930fe8c345a1aa6ee8f05288250596a1e9b48c2
ReplyDev <replydev@protonmail.com>
2020-12-04 11:02:45 +0100

    Save work

--- app/src/main/java/me/replydev/notes_android/activities/NotesActivity.kt
+++ app/src/main/java/me/replydev/notes_android/activities/NotesActivity.kt
@@ -3,14 +3,15 @@ package me.replydev.notes_android.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import me.replydev.notes_android.Globals
 import me.replydev.notes_android.R
 import me.replydev.notes_android.activities.adapters.NotesAdapter
-import me.replydev.notes_android.runnables.DecryptNotes
 import me.replydev.notes_android.json.EncryptedNote
 import me.replydev.notes_android.json.Note
+import me.replydev.notes_android.runnables.DecryptNotes
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlin.properties.Delegates
@@ -40,6 +41,16 @@ class NotesActivity : AppCompatActivity(){
         )
         val notesListView = findViewById<ListView>(R.id.notesListView)
         notesListView.adapter = notesAdapter
+
+        notesListView.setOnItemClickListener { adapterView: AdapterView<*>, view1: View, i: Int, l: Long ->
+            val note: Note = adapterView.getItemAtPosition(i) as Note
+            val intent = Intent(this, WriteActivity::class.java).apply {
+                putExtra("ID", note.id)
+                putExtra("JSON", note.toJson())
+            }
+            startActivityForResult(intent, 1)
+            //println(note)
+        }
         executorService.execute(
             DecryptNotes(
                 encryptedNotes,
@@ -63,7 +74,7 @@ class NotesActivity : AppCompatActivity(){
         val lastId = if(notes.size == 0){
             0
         } else{
-            notes[notes.size - 1].id
+            notes[notes.size - 1].id + 1
         }
         val intent = Intent(this, WriteActivity::class.java).apply {
             putExtra("ID", lastId)
@@ -78,11 +89,21 @@ class NotesActivity : AppCompatActivity(){
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1) {
-            val noteJson = data?.getStringExtra("NOTE")
+        if (resultCode == 1) { //add new note
+            val noteJson = data?.getStringExtra("NOTE") ?: return //return if data?.getStringExtra("NOTE") is null
             val note: Note = Globals.gson.fromJson(noteJson, Note::class.java)
             notes.add(note)
-            notesAdapter.notifyDataSetChanged()
         }
+        else if(resultCode == 2){  //update existing note
+            val noteJson = data?.getStringExtra("NOTE") ?: return //return if data?.getStringExtra("NOTE") is null
+            val note: Note = Globals.gson.fromJson(noteJson, Note::class.java)
+            for (i in 0 until notes.size) {
+                if(note.id == notes[i].id){
+                    notes[i] = note
+                    break
+                }
+            }
+        }
+        notesAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/me/replydev/notes_android/activities/WriteActivity.kt
+++ app/src/main/java/me/replydev/notes_android/activities/WriteActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import me.replydev.notes_android.Globals
 import me.replydev.notes_android.R
 import me.replydev.notes_android.json.Note
 import me.replydev.notes_android.runnables.SendNoteToServer
@@ -17,6 +18,7 @@ class WriteActivity : AppCompatActivity() {
 
     private var id by Delegates.notNull<Int>()
     private var userId by Delegates.notNull<Int>()
+    private lateinit var noteJson: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +26,55 @@ class WriteActivity : AppCompatActivity() {
 
         id = intent.getIntExtra("ID",-1)
         userId = intent.getIntExtra("USER_ID",-1)
+        noteJson = intent.getStringExtra("JSON").toString()
+        if(noteJson != "null"){  //update existing note
+            val note = Globals.gson.fromJson(noteJson,Note::class.java)
+            val titleAddNoteEditText = findViewById<EditText>(R.id.titleAddNoteEditText)
+            val bodyAddNoteEditText = findViewById<EditText>(R.id.bodyAddNoteEditText)
+            titleAddNoteEditText.setText(note.title)
+            bodyAddNoteEditText.setText(note.body)
+            id = note.id
+            userId = note.authorId
+        }
     }
 
-    fun saveNewNote(view: View){
+    fun executeOperation(view: View){
+        if(noteJson == "null"){
+            buildNoteAndSend(1)
+        }
+        else{
+            buildNoteAndSend(2)
+        }
+    }
+
+    /*private fun updateNote(){
+        val titleAddNoteEditText = findViewById<EditText>(R.id.titleAddNoteEditText)
+        val bodyAddNoteEditText = findViewById<EditText>(R.id.bodyAddNoteEditText)
+        val randomSaltBytes = ByteArray(32)
+        SecureRandom().nextBytes(randomSaltBytes)
+        val saltBase64 = Base64.getEncoder().encodeToString(randomSaltBytes)
+        val note =  Note(id,userId,saltBase64,titleAddNoteEditText.text.toString(),bodyAddNoteEditText.text.toString())
+        val resultCode = 2
+        val resultIntent = Intent()
+        resultIntent.putExtra("NOTE", note.toJson())
+        setResult(resultCode, resultIntent)
+        val executorService = Executors.newSingleThreadExecutor()
+        executorService.execute(SendNoteToServer(note))
+        finish()
+    }*/
+
+    private fun buildNoteAndSend(resultId: Int){
         val titleAddNoteEditText = findViewById<EditText>(R.id.titleAddNoteEditText)
         val bodyAddNoteEditText = findViewById<EditText>(R.id.bodyAddNoteEditText)
         val randomSaltBytes = ByteArray(32)
         SecureRandom().nextBytes(randomSaltBytes)
         val saltBase64 = Base64.getEncoder().encodeToString(randomSaltBytes)
         val note = Note(id,userId,saltBase64,titleAddNoteEditText.text.toString(),bodyAddNoteEditText.text.toString())
-        val resultCode = 1;
         val resultIntent = Intent()
         resultIntent.putExtra("NOTE", note.toJson())
-        setResult(resultCode, resultIntent)
-
-
+        setResult(resultId, resultIntent)
         val executorService = Executors.newSingleThreadExecutor()
-        executorService.submit(SendNoteToServer(note))
+        executorService.execute(SendNoteToServer(note))
         finish()
     }
 }
